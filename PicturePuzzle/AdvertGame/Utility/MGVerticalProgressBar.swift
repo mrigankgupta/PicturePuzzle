6d1d1054a16abe3dd206a2713f5b1989fb6d7262
Mrigank Gupta <mrigankgupta@gmail.com>
2017-08-21 22:00:57 +0530

    add image support for progress layer

--- PicturePuzzle/AdvertGame/Utility/MGVerticalProgressBar.swift
+++ PicturePuzzle/AdvertGame/Utility/MGVerticalProgressBar.swift
@@ -12,10 +12,12 @@ import UIKit
 class MGVerticalProgressBar: UIView {
     fileprivate var progressLayer: CAShapeLayer?
     private var textLayer: CATextLayer?
+    private var innerImageLayer: CAShapeLayer?
     private var innerProgress: Float = 0.5 {
         didSet {
             if let progressLayer = progressLayer {
-                animateLayer(progressLayer, with: bounds)
+                animateLayer(progressLayer, rect: bounds, progress: progress,
+                             insetX: insetX, insetY: insetY, cornerRadius: cornerRadius)
             }
         }
     }
@@ -68,8 +70,9 @@ class MGVerticalProgressBar: UIView {
     
     @IBInspectable public var fillImage: UIImage? {
         didSet {
-            progressLayer?.contents = fillImage?.cgImage
-            progressLayer?.fillColor = UIColor.clear.cgColor
+            innerImageLayer?.contents = fillImage?.cgImage
+            progressLayer?.fillColor = UIColor.white.cgColor
+            innerImageLayer!.mask = progressLayer
         }
     }
     
@@ -122,86 +125,106 @@ class MGVerticalProgressBar: UIView {
         super.init(frame: frame)
         configureTrackBar()
         configureProgressTrack()
-        layer.addSublayer(textLayer!)
+        configureTextLayer()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         configureTrackBar()
         configureProgressTrack()
+        configureTextLayer()
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        shapeTextLayer(frame: bounds)
-        shapeProgressLayer(frame: bounds)
+        adjustTextLayer(frame: bounds)
+        adjustImageLayer(frame: bounds)
+        adjustProgressLayer(frame: bounds)
     }
 }
 
 private extension MGVerticalProgressBar {
     
+    func adjustTextLayer(frame:CGRect) {
+        textLayer?.frame = getBottomSquareFrame(frame: bounds, insetX: insetX, insetY: insetY)
+    }
+    
+    func adjustProgressLayer(frame:CGRect) {
+        progressLayer?.path = getRectangularBezierPath(forRect: bounds, progress: progress,
+                                                       insetX: insetX, insetY: insetY, cornerRadius: cornerRadius).cgPath
+        progressLayer?.frame = bounds
+    }
+    
+    func adjustImageLayer(frame:CGRect) {
+        innerImageLayer?.path = getRectangularBezierPath(forRect: bounds, progress: 1.0,
+                                                         insetX: insetX, insetY: insetY, cornerRadius: cornerRadius).cgPath
+        innerImageLayer?.frame = bounds
+    }
+
     func configureTrackBar() {
         layerProperties()
         layer.backgroundColor = trackColor.cgColor
     }
     
     func configureProgressTrack() {
+        if innerImageLayer == nil {
+            innerImageLayer = createInnerLayer(frame: bounds, progress: progress,
+                                               insetX: insetX, insetY: insetY, cornerRadius: cornerRadius, fillColor: UIColor.clear)
+            layer.addSublayer(innerImageLayer!)
+        }
         if progressLayer == nil {
-            createProgressLayer(frame: bounds)
+            progressLayer = createInnerLayer(frame: bounds, progress: progress,
+                                             insetX: insetX, insetY: insetY, cornerRadius: cornerRadius, fillColor: fillColor)
             layer.addSublayer(progressLayer!)
         }
-        if textLayer == nil && progressLayer != nil {
-            createTextLayer(frame: bounds)
+    }
+    
+    func configureTextLayer() {
+        if textLayer == nil {
+            textLayer = createTextLayer(frame: bounds, insetX: insetX, insetY: insetY)
             layer.addSublayer(textLayer!)
         }
     }
     
-    func createProgressLayer(frame:CGRect) {
-        progressLayer = CAShapeLayer()
-        shapeProgressLayer(frame: frame)
-        progressLayer!.fillColor = fillColor.cgColor
-    }
-    
-    func shapeProgressLayer(frame:CGRect) {
-        progressLayer?.frame = frame
-        progressLayer?.path = getRectangularBezierPath(forRect: frame, progress: progress,
+    func createInnerLayer(frame:CGRect, progress:Float, insetX:Float, insetY:Float, cornerRadius:Float, fillColor:UIColor)-> CAShapeLayer {
+        let layer = CAShapeLayer()
+        layer.path = getRectangularBezierPath(forRect: frame, progress: progress,
                                                        insetX: insetX, insetY: insetY, cornerRadius: cornerRadius).cgPath
+        layer.fillColor = fillColor.cgColor
+        return layer
     }
     
-    func createTextLayer(frame:CGRect) {
-        textLayer = CATextLayer()
-        shapeTextLayer(frame: frame)
-        textLayer!.alignmentMode = kCAAlignmentCenter
-        textLayer!.isWrapped = true
+    func createTextLayer(frame:CGRect, insetX:Float, insetY:Float)-> CATextLayer {
+        let layer = CATextLayer()
+        layer.frame = getBottomSquareFrame(frame: frame, insetX:insetX, insetY:insetY)
+        layer.alignmentMode = kCAAlignmentCenter
+        layer.isWrapped = true
+        return layer
     }
     
-    func shapeTextLayer(frame:CGRect) {
+    func getBottomSquareFrame(frame:CGRect, insetX:Float, insetY:Float)-> CGRect {
         let fullRectInset = frame.insetBy(dx: CGFloat(insetX), dy: CGFloat(insetY))
-        let textFrame = CGRect(x: fullRectInset.origin.x, y: fullRectInset.origin.y + fullRectInset.size.height - fullRectInset.size.width,
+        let newFrame = CGRect(x: fullRectInset.origin.x, y: fullRectInset.origin.y + fullRectInset.size.height - fullRectInset.size.width,
                                width: fullRectInset.size.width, height: fullRectInset.size.width)
-        textLayer?.frame = textFrame
+        return newFrame
     }
     
-    func animateLayer(_ layer:CAShapeLayer, with rect:CGRect) {
+    func animateLayer(_ layer:CAShapeLayer, rect:CGRect, progress:Float, insetX:Float, insetY:Float, cornerRadius:Float) {
         let basicAnimation = CABasicAnimation(keyPath: "path")
         let currentPresentationLayer = layer.presentation()
         if let oldBezierPath = currentPresentationLayer?.path {
             let finalBezeirPath = getRectangularBezierPath(forRect: rect, progress: progress,
-                                                           insetX: insetX, insetY: insetY, cornerRadius: cornerRadius)
+                                                           insetX: insetX, insetY: insetY, cornerRadius: cornerRadius).cgPath
             basicAnimation.fromValue = oldBezierPath
-            basicAnimation.toValue = finalBezeirPath.cgPath
+            basicAnimation.toValue = finalBezeirPath
             basicAnimation.duration = 2.0
-            basicAnimation.fillMode = kCAFillModeBoth
-            basicAnimation.delegate = self
-            layer.path = finalBezeirPath.cgPath
-            if fillImage != nil {
-                layer.mask = finalBezeirPath.path
-            }
+            basicAnimation.timingFunction = CAMediaTimingFunction(name:kCAMediaTimingFunctionEaseInEaseOut)
+            layer.path = finalBezeirPath
             layer.add(basicAnimation, forKey: "progress")
         }
     }
     
-    func getRectangularBezierPath(forRect rect:CGRect, progress:Float, insetX:Float, insetY:Float, cornerRadius:Float) -> UIBezierPath {
+    func getRectangularBezierPath(forRect rect:CGRect, progress:Float, insetX:Float, insetY:Float, cornerRadius:Float)-> UIBezierPath {
         let fullRectInset = rect.insetBy(dx: CGFloat(insetX), dy: CGFloat(insetY))
         let progressHeight = fullRectInset.size.height*CGFloat(progress)
         let progressRect = CGRect(x: fullRectInset.origin.x, y: fullRectInset.origin.y + fullRectInset.size.height - progressHeight,
@@ -210,7 +233,3 @@ private extension MGVerticalProgressBar {
     }
 }
 
-extension MGVerticalProgressBar: CAAnimationDelegate {
-    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
-    }
-}
